d47105160c31e94acd8e1cbc85e56b155eeb19d0
Michel <106674127+MichelZero@users.noreply.github.com>
2024-09-26 08:35:37 -0300

    up

--- a.py
+++ a.py
@@ -2,23 +2,30 @@
 # autor: michel
 # data: 25/09/2024
 
-# 
+# criando uma classe de exemplo 
 class A:
   
   def __init__(self, value):
-    self._b = value
+    self._b = value # atributo protegido da classe A que pode 
+    # ser acessado por outras classes que a herdam ou instanciam 
   
   #get
   def getB(self):
-    return self._b
+    return self._b # retorna o valor do atributo protegido _b da classe A 
   
   # set
   def setB(self, algo):
+    if algo % 2 == 0:
+      self._b = algo # atribui um novo valor ao atributo protegido _b da classe A
+    else:
+      print(f"Erro: foi informado {algo}, informe um valor par!")
+
     
 
-a1 = A(2)
-print(f"valor de _b -> {a1._b}")
-print(f"get1 de _b -> {a1.getB()}")
-a1._b = 5
-a1.setB(3)
-print(f"get2 de _b -> {a1.getB()}")
+a1 = A(2) # instanciando a classe A com o valor 2 para o atributo protegido _b da classe A
+print(f"valor de _b -> {a1._b}") # acessando o atributo protegido _b da classe A de forma direta
+print(f"get1 de _b -> {a1.getB()}") # acessando o atributo protegido _b da classe A de forma indireta
+a1._b = 5 # alterando o valor do atributo protegido _b da classe A de forma direta 
+a1.setB(3) # chamando o set para alterar o valor do atributo protegido _b da classe A
+a1.setB(3) # chamando o set para alterar o valor do atributo protegido _b da classe A
+print(f"get2 de _b -> {a1.getB()}") # acessando o atributo protegido _b da classe A de forma indireta
--- b.py
+++ b.py
@@ -1,28 +1,36 @@
 #
-# autor: michel
+# autor: Michel
 # data: 25/09/2024
 
-# 
+# criando uma classe de exemplo
 class B:
-  def __init__(self, value):
-    self.__a = value
+  def __init__(self, value): 
+    self.__a = value # atributo privado da classe B que só pode ser acessado por 
+    # ela mesma e não por outras classes que a herdam ou instanciam
+    self._b = 2 # atributo protegido da classe B que pode ser acessado por 
+    # outras classes que a herdam ou instanciam    
     
   #get
-  @property
-  def a(self):
-    print("vc uso get")
-    return self.__a
+  @property # decorator que permite acessar o atributo privado da classe B
+  def a(self):  
+    print("vc uso get") # mensagem de teste para verificar se o get foi chamado
+    return self.__a # retorna o valor do atributo privado __a da classe B 
   
   #set
   @a.setter
   def a(self, algo):
     if algo % 2 == 0:
-      self.__a = algo
+      self.__a = algo # atribui um novo valor ao atributo privado __a da classe B 
     else:
       print(f"Erro: foi informado {algo}, informe um valor par!")
     
 
-b1 = B(2)
-#print(f"print no a: -> {b1.__a}")
-print(f"print no a: -> {b1.a}")
-b1.a = 3    
+b1 = B(2) # instanciando a classe B com o valor 2 para o atributo privado __a da classe B
+# print(f"print no a: -> {b1.__a}") # erro, pois o atributo __a é privado e não pode ser acessado 
+# diretamente por outras classes que a herdam ou instanciam 
+
+print(f"print no a: -> {b1._B__a}") # acessando o atributo privado __a da classe B de forma indireta  
+print(f"print no a: -> {b1.a}") # acessando o atributo privado __a da classe B de forma direta 
+b1.a = 3     # chamando o set para alterar o valor do atributo privado __a da classe B
+b1.a = 4     # chamando o set para alterar o valor do atributo privado __a da classe B
+print(f"print no a: -> {b1.a}") # acessando o atributo privado __a da classe B de forma direta
